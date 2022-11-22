4c3eb264aab1db851e14c4a469b750d7bc485446
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-11-23 05:17:51 +0600

    Authentication part completed

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtqu1wZiLNtTid99TfAid-sw8y6-WsjhY",
+  authDomain: "air-cnc-31c3c.firebaseapp.com",
+  projectId: "air-cnc-31c3c",
+  storageBucket: "air-cnc-31c3c.appspot.com",
+  messagingSenderId: "1030214716852",
+  appId: "1:1030214716852:web:7fc2834a7ae63dba7c36e1"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
